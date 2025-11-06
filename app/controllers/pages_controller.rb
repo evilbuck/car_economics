cd050bc62dedda765853d64606eb44aed0abe390
Evil Buck <buckleyrobinson@gmail.com>
2025-11-06 16:34:56 -0500

    root route & safe guarding the custom pages route

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,17 @@
 class PagesController < ApplicationController
+  ALLOWED_PAGES = %w[mpg_calculator]
+
   def get_page_by_name
     page = params[:page_name]
 
+    raise ActionController::RoutingError, 'Not Found' unless is_allowed(page)
+
     render page
+  end
+
+  private
+
+  def is_allowed(page)
+    return ALLOWED_PAGES.include?(page)
   end
 end
